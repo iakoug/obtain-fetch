4f8546f5ef89a53786e2b48802136a94a8067d0b
christian <baozaodexianyu@163.com>
2019-04-01 11:30:25 +0800

    feat: update test js

--- example.test.js
+++ example.test.js
@@ -1,19 +1,21 @@
 // http://localhost:4000/banner
 // import obtain from './'
-const { obtain, curl } = require('./')
+const obtain = require('./')
 
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-obtain.interceptor.request.use(option => {
-  console.log('in to interceptor ****************', option)
-  return option
-}, 123)
-obtain.interceptor.response.use(option => {
-  console.log('after interceptor ****************', option)
-  return option
-}, 123)
+obtain.use(function(http) {
+  http.interceptor.request.use(option => {
+    console.log('in to interceptor ****************', option)
+    return option
+  }, 123)
+  http.interceptor.response.use(option => {
+    console.log('after interceptor ****************', option)
+    return option
+  }, 123)
+})
 
-curl('http://localhost:4000/banner').then(async res => {
+obtain('http://localhost:4000/banner').then(async res => {
   console.log(res, '1')
 })
 
